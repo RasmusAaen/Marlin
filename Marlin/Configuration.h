c51dc1593df5d905dc79ba1064866a071018178d
Rasmus Aaen <rasmus@aaen.net>
2020-05-12 08:34:22 +0200

    Glass bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1084,11 +1084,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 520
-#define Y_BED_SIZE 513
+#define X_BED_SIZE 490
+#define Y_BED_SIZE 490
 
-// Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -11
+// Travel limit
+// (mm) after homing, corresponding to endstop positions.
+#define X_MIN_POS -26
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
